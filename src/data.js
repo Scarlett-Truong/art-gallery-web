a7328a6ea73cd1545aa8d17ada08c4dcf07cbdd7
Scarlett Truong <43763644+Scarlett-DMG@users.noreply.github.com>
2019-11-02 18:34:36 -0700

    Move server outside

---
+++ src/data.js
@@ -0,0 +1,77 @@
+const products = [
+    {
+      id: 01,
+      name: 'Product 1',
+      available_quantity: 5,
+      price: 450,
+      pic_url: 'https://picsum.photos/id/1005/200/300',
+      description: 'Lorem ipsum dolor sit amet, iusto appellantur vix te, nam affert feugait menandri eu. Magna simul ad est. Nostrum neglegentur ius at, at pertinax repudiare vel. Vim an adolescens quaerendum.'
+
+    },
+  
+    {
+      id: 02,
+      name: 'Product 2',
+      available_quantity: 7,
+      price: 50,
+      pic_url: 'https://picsum.photos/id/102/200/300',
+      description: 'Lorem ipsum dolor sit amet, iusto appellantur vix te, nam affert feugait menandri eu. Magna simul ad est. Nostrum neglegentur ius at, at pertinax repudiare vel. Vim an adolescens quaerendum.'
+    },
+  
+    {
+      id: 03,
+      name: 'Product 3',
+      available_quantity: 0,
+      price: 500,
+      pic_url: 'https://picsum.photos/id/1048/200/300',
+      description: 'Lorem ipsum dolor sit amet, iusto appellantur vix te, nam affert feugait menandri eu. Magna simul ad est. Nostrum neglegentur ius at, at pertinax repudiare vel. Vim an adolescens quaerendum.'
+    },
+  
+    {
+      id: 04,
+      name: 'Product 4',
+      available_quantity: 4,
+      price: 1500,
+      pic_url: 'https://picsum.photos/id/1026/200/300',
+      description: 'Lorem ipsum dolor sit amet, iusto appellantur vix te, nam affert feugait menandri eu. Magna simul ad est. Nostrum neglegentur ius at, at pertinax repudiare vel. Vim an adolescens quaerendum.'
+    },
+    {
+      id: 05,
+      name: 'Product 5',
+      available_quantity: 5,
+      price: 450,
+      pic_url: 'https://picsum.photos/id/1027/200/300',
+      description: 'Lorem ipsum dolor sit amet, iusto appellantur vix te, nam affert feugait menandri eu. Magna simul ad est. Nostrum neglegentur ius at, at pertinax repudiare vel. Vim an adolescens quaerendum.'
+
+    },
+  
+    {
+      id: 06,
+      name: 'Product 6',
+      available_quantity: 7,
+      price: 50,
+      pic_url: 'https://picsum.photos/id/1050/200/300',
+      description: 'Lorem ipsum dolor sit amet, iusto appellantur vix te, nam affert feugait menandri eu. Magna simul ad est. Nostrum neglegentur ius at, at pertinax repudiare vel. Vim an adolescens quaerendum.'
+    },
+  
+    {
+      id: 07,
+      name: 'Product 7',
+      available_quantity: 9,
+      price: 500,
+      pic_url: 'https://picsum.photos/id/1047/200/300',
+      description: 'Lorem ipsum dolor sit amet, iusto appellantur vix te, nam affert feugait menandri eu. Magna simul ad est. Nostrum neglegentur ius at, at pertinax repudiare vel. Vim an adolescens quaerendum.'
+    },
+  
+    {
+      id: 08,
+      name: 'Product 8',
+      available_quantity: 4,
+      price: 1500,
+      pic_url: 'https://picsum.photos/id/1071/200/300',
+      description: 'Lorem ipsum dolor sit amet, iusto appellantur vix te, nam affert feugait menandri eu. Magna simul ad est. Nostrum neglegentur ius at, at pertinax repudiare vel. Vim an adolescens quaerendum.'
+    },
+  ];
+
+  
+  module.exports = { products: products }
---
+++ src/server.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const data = require('./data');
+const path = require("path");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/products', (req, res) => {
+  return res.json(data.products);
+});
+
+app.use(express.static(path.join(__dirname, "build")))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+
+app.post('/products', (req, res) => {
+  let products = [], id = null;
+  let cart = JSON.parse(req.body.cart);
+  if (!cart) return res.json(products)
+  for (var i = 0; i < data.products.length; i++) {
+    id = data.products[i].id.toString();
+    if (cart.hasOwnProperty(id)) {
+      data.products[i].qty = cart[id]
+      products.push(data.products[i]);
+    }
+  }
+  return res.json(products);
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT);
+console.log('Server is runnging on port ' + PORT + ': ');
